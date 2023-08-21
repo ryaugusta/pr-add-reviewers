b041ba760c62e33ba41924951c590d5c2e450538
ryaugusta <ryaugusta@gmail.com>
2023-08-21 14:02:44 -0400

    fixing issues

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    try {
-        const token = core.getInput('token', {required: true});
-        const reviewers = core.getInput('reviewers'); 
-        const user_reviewers = reviewers.split(',');
-        const team_reviewers = core.getInput('team_reviewers'); 
-        const slug_reviewers = team_reviewers.split(',');
-        const octokit = new github.getOctokit(token); 
-        const context = github.context;
-        const owner = context.repo.owner;
-        const repo = context.repo.repo;
-        const pr_number = context.payload.pull_request.number;
-        
-        const params = {
-            owner: owner,
-            repo: repo,
-            pr_number: pr_number
-        }
+    const token = core.getInput('token', {required: true});
+    const reviewers = core.getInput('reviewers'); 
+    const user_reviewers = reviewers.split(',');
+    const team_reviewers = core.getInput('team_reviewers'); 
+    const slug_reviewers = team_reviewers.split(',');
+    const octokit = new github.getOctokit(token); 
+    const context = github.context;
+    const owner = context.repo.owner;
+    const repo = context.repo.repo;
+    const pr_number = context.payload.pull_request.number;
 
+    const params = {
+        owner: owner,
+        repo: repo,
+        pr_number: pr_number,
+    };
+
+    try {
+        
         if (context.payload.pull_request == null) {
             core.setFailed("No pull request found.");
             return;
