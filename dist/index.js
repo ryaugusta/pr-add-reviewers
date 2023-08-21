64bfe098749718065600db3631e8245a0ea2b3bf
ryaugusta <ryaugusta@gmail.com>
2023-08-21 13:25:33 -0400

    cleaning up and making owner/repo the working repo

--- dist/index.js
+++ dist/index.js
@@ -9817,46 +9817,45 @@ const github = __nccwpck_require__(1908);
 async function run() {
     try {
         const token = core.getInput('token', {required: true});
-        const owner = core.getInput('owner', {required: true}); 
-        const repo = core.getInput('repo', {required: true}); 
         const reviewers = core.getInput('reviewers'); 
         const user_reviewers = reviewers.split(',');
         const team_reviewers = core.getInput('team_reviewers'); 
         const slug_reviewers = team_reviewers.split(',');
-
         const octokit = new github.getOctokit(token); 
         const context = github.context;
+        const owner = context.repo.owner;
+        const repo = context.repo.repo;
+        const pr_number = context.payload.pull_request.number;
+        
+        const params = {
+            owner: owner,
+            repo: repo,
+            pr_number: pr_number
+        }
         
         if (context.payload.pull_request == null) {
             core.setFailed("No pull request found.");
             return;
         }
         
-        const pr_number = context.payload.pull_request.number;
     
         if(reviewers != '') {
             await octokit.rest.pulls.requestReviewers({
-                owner: owner,
-                repo: repo,
-                pull_number: pr_number,
+                params,
                 reviewers: user_reviewers
             });
         }
         
         else if(team_reviewers != '') {
             await octokit.rest.pulls.requestReviewers({
-                owner: owner,
-                repo: repo,
-                pull_number: pr_number,
+                params,
                 team_reviewers: slug_reviewers
             });
         }
 
         else {
             await octokit.rest.pulls.requestReviewers({
-                owner: owner,
-                repo: repo,
-                pull_number: pr_number,
+                params,
                 reviewers: user_reviewers,
                 team_reviewers: slug_reviewers
             });
